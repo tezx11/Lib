7965d397721410384ab2e18598e147d07fa8060d
tezx11 <164819345+tezx11@users.noreply.github.com>
2024-06-20 23:07:49 +0200

    Update VisualsTab.lua

--- Tabs/VisualsTab.lua
+++ Tabs/VisualsTab.lua
@@ -1,1 +1,5 @@
+local repo = "https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/"
 
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tezx11/Lib/main/Library.lua"))()
+local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/tezx11/Lib/main/ThemeManager"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/tezx11/Lib/main/SaveManager"))()
