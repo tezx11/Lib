1bc4d9a8bc0f71ff1d07c2f0802d956033897046
tezx11 <164819345+tezx11@users.noreply.github.com>
2024-06-20 23:40:41 +0200

    Delete Tabs directory

--- Tabs/VisualsTab.lua
+++
@@ -1,5 +0,0 @@
-local repo = "https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/"
-
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tezx11/Lib/main/Library.lua"))()
-local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/tezx11/Lib/main/ThemeManager"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/tezx11/Lib/main/SaveManager"))()
